5b7258b0b37573301d06b1cfd252846a452a23cd
15871400698 <1095977792@qq.com>
2023-11-08 16:09:31 +0800

    初始提交

---
+++ src/main/java/com/fxgz/gittest/Main.java
@@ -0,0 +1,16 @@
+package com.fxgz.gittest;
+
+/**
+ * @author Fuxing Deng
+ * @date on 2023/11/8 15:40
+ */
+public class Main {
+
+    public static void helloV1(){
+        System.out.println("2023-11-08");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
